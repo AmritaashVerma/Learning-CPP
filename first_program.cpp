470a29e7d778a1b6b059d09e4dd6d98de2e6249d
Amritaash Verma <amritaash.verma@gmail.com>
2025-08-31 08:36:03 +0000

    inti2

---
+++ first_program.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int fav_num; 
+
+    cout<<"Enter your favourite number: ";
+    cin >> fav_num;
+
+    cout<<fav_num;
+
+    return 0;
+}
